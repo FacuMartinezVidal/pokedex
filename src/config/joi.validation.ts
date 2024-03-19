335cc0f4359aec109a384b7c9b0e5699ed477930
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2024-03-18 21:30:41 -0300

    deployment

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,7 +1,6 @@
 import * as Joi from 'joi';
 
 export const JoiValidation = Joi.object({
-  NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
   MONGODB: Joi.string().required(),
   PORT: Joi.number().default(3001),
   DEFAULT_LIMIT: Joi.number().default(10),
